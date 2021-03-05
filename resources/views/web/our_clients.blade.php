ca76f4d38da41c99540c0572c6c9bdbe3881bee0
Natwisit <stampcsmju@gmail.com>
2021-03-05 10:38:18 +0700

    remove .idea

--- resources/views/web/our_clients.blade.php
+++ resources/views/web/our_clients.blade.php
@@ -52,36 +52,12 @@
             font-weight: bold;
         }
 
-
-        .pagination,
-        .jsgrid .jsgrid-pager {
-            display: flex;
-            padding-left: 0;
-            list-style: none;
-            border-radius: 0.25rem
-        }
-
+        /**, ::after, ::before {*/
+        /*    box-sizing: border-box;*/
+        /*    border-radius: 50px;*/
+        /*}*/
         .page-link {
-            color: black
-        }
-
-        .pagination.pagination-rounded-flat .page-item {
-            margin: 0 .25rem
-        }
-
-        .pagination-rounded-flat {
-        }
-
-        .pagination-success .page-item.active .page-link {
-            background: #007AE8;
-            border-color: #007AE8;
-            color: white;
-            width: 30px;
-        }
-
-        .pagination.pagination-rounded-flat .page-item .page-link {
-            border: none;
-            border-radius: 50px
+            border-radius: 50px;
         }
 
     </style>
@@ -314,21 +290,25 @@
 
                     <div class="row">
                         <div class="col-12 d-flex justify-content-end">
-                            <ul class="pagination d-flex justify-content-center flex-wrap pagination-rounded-flat pagination-success">
-                                <li class="page-item"><a class="page-link" href="#" data-abc="true"><span
-                                            aria-hidden="true">&laquo;</span>
-                                        <span class="sr-only">Previous</span></a></li>
-                                <li class="page-item"><a class="page-link" href="#" data-abc="true"><</a></li>
-                                <li class="page-item active"><a class="page-link" href="#" data-abc="true">1</a>
-                                </li>
-                                <li class="page-item"><a class="page-link" href="#" data-abc="true">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#" data-abc="true">3</a></li>
-                                <li class="page-item"><a class="page-link" href="#" data-abc="true">4</a></li>
-                                <li class="page-item"><a class="page-link" href="#" data-abc="true">></a></li>
-                                <li class="page-item"><a class="page-link" href="#" data-abc="true"><span
-                                            aria-hidden="true">&raquo;</span>
-                                        <span class="sr-only">Next</span></a></li>
-                            </ul>
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination justify-content-end">
+                                    <li class="page-item disabled">
+                                        <a class="page-link" href="#" tabindex="-1"><<</a>
+                                    </li>
+                                    <li class="page-item disabled">
+                                        <a class="page-link" href="#" tabindex="-1"><</a>
+                                    </li>
+                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">></a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">>></a>
+                                    </li>
+                                </ul>
+                            </nav>
                         </div>
                     </div>
                 </div>
